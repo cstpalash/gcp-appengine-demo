21dd015d52e590498e6ef4ee5ce65c91f66c3c27
Palash Roy <cstpalash@gmail.com>
2020-08-02 13:59:24 +0530

    build push and deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I am a simple react app, running inside GCP AppEngine. Happy learning.
+          I am a simple react app, running inside GCP AppEngine.
         </p>
         <a
           className="App-link"
